cb1d60d578ecf73f1d4945520e23a023c15ac0e2
gitsupportjog <jog.dmlqa1@gmail.com>
2026-05-18 15:20:26 +0700

    Add files via upload

---
+++ First Launch.air/Launch.air/Launch.py
@@ -0,0 +1,26 @@
+# -*- encoding=utf8 -*-
+__author__ = "USER"
+
+from airtest.core.api import *
+
+auto_setup(__file__)
+touch(Template(r"tpl1773384581607.png", record_pos=(0.343, -0.053), resolution=(828, 1792)), times=2)
+sleep(15)
+touch(Template(r"tpl1773384672234.png", record_pos=(0.35, 0.175), resolution=(1792, 828)))
+sleep(2)
+touch(Template(r"tpl1773384695785.png", record_pos=(-0.218, 0.027), resolution=(1792, 828)))
+touch(Template(r"tpl1773384705771.png", record_pos=(0.031, -0.034), resolution=(1792, 828)))
+touch(Template(r"tpl1773384731338.png", record_pos=(0.405, 0.177), resolution=(1792, 828)))
+sleep(2)
+touch(Template(r"tpl1773384771955.png", record_pos=(-0.379, 0.186), resolution=(1792, 828)))
+sleep(5)
+touch(Template(r"tpl1773384796276.png", record_pos=(0.41, 0.193), resolution=(1792, 828)))
+touch(Template(r"tpl1773384816193.png", record_pos=(-0.072, 0.179), resolution=(1792, 828)))
+text("a")
+sleep(1)
+touch(Template(r"tpl1773384848638.png", record_pos=(0.416, 0.177), resolution=(1792, 828)))
+sleep(4)
+touch(Template(r"tpl1773384886464.png", record_pos=(-0.099, 0.022), resolution=(1792, 828)))
+touch(Template(r"tpl1773384939099.png", record_pos=(0.003, -0.012), resolution=(1792, 828)))
+sleep(4)
+swipe(Template(r"tpl1773384967291.png", record_pos=(0.042, -0.001), resolution=(1792, 828)), vector=[-0.0083, -0.3335], times=4)
