c33f04cb0582bf73f61f81983494bf4e9f84e0e9
gitsupportjog <jog.dmlqa1@gmail.com>
2026-05-19 14:31:20 +0700

    Add files via upload

---
+++ Codeplaydu_Minigame/Codeplaydu_Minigame/Codeplaydu_Minigame_Lvl2.air/Codeplaydu_Minigame_Lvl2.py
@@ -0,0 +1,36 @@
+# -*- encoding=utf8 -*-
+__author__ = "albam"
+
+from airtest.core.api import *
+
+auto_setup(__file__)
+while True:
+    if exists(Template(r"tpl1775529415334.png", record_pos=(-0.235, -0.051), resolution=(1334, 750))):
+        touch(Template(r"tpl1775529415334.png", record_pos=(-0.235, -0.051), resolution=(1334, 750)))
+        sleep(1.0)
+    else:
+        break
+while True:
+    if exists(Template(r"tpl1775529623551.png", record_pos=(-0.185, -0.196), resolution=(1334, 750))):
+        touch(Template(r"tpl1775529634160.png", record_pos=(-0.241, -0.043), resolution=(1334, 750)))
+        sleep(3.0)
+    else:
+        break
+# STAGE LEVEL 2
+exists(Template(r"tpl1775530298499.png", record_pos=(-0.009, 0.142), resolution=(1334, 750)))
+touch(Template(r"tpl1775530298499.png", record_pos=(-0.009, 0.142), resolution=(1334, 750)))
+wait(Template(r"tpl1775529723787.png", record_pos=(-0.001, 0.123), resolution=(1334, 750)))
+for i in range(2):
+    touch(Template(r"tpl1775529723787.png", record_pos=(-0.001, 0.123), resolution=(1334, 750)))
+touch(Template(r"tpl1775529828243.png", record_pos=(0.38, 0.201), resolution=(1334, 750)))
+sleep(10.0)
+while True:
+    hand = exists(Template(r"tpl1775535603623.png", record_pos=(0.067, 0.078), resolution=(1334, 750)))
+    if hand:
+        touch(hand)
+        sleep(5.0)
+    else:
+        break
+sleep(3.0)
+if exists(Template(r"tpl1775035986422.png", record_pos=(0.438, 0.212), resolution=(1334, 750))):
+    touch(Template(r"tpl1775035986422.png", record_pos=(0.438, 0.212), resolution=(1334, 750)))
