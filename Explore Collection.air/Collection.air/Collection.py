e7c429a76278eb92043e32c41aeb1a7939c68a20
gitsupportjog <jog.dmlqa1@gmail.com>
2026-05-19 14:47:58 +0700

    Add files via upload

---
+++ Explore Collection.air/Collection.air/Collection.py
@@ -0,0 +1,70 @@
+# -*- encoding=utf8 -*-
+__author__ = "alba.millentry"
+
+from airtest.core.api import *
+
+auto_setup(__file__)
+wait(Template(r"tpl1775717608079.png", record_pos=(-0.444, -0.214), resolution=(1334, 750)))
+touch(Template(r"tpl1775717608079.png", record_pos=(-0.444, -0.214), resolution=(1334, 750)))
+wait(Template(r"tpl1775717646912.png", record_pos=(-0.283, -0.237), resolution=(1334, 750)))
+# enter all collection obtained
+exists(Template(r"tpl1775717677253.png", record_pos=(-0.313, -0.172), resolution=(1334, 750)))
+touch(Template(r"tpl1775717677253.png", record_pos=(-0.313, -0.172), resolution=(1334, 750)))
+swipe(Template(r"tpl1775717971109.png", target_pos=8, record_pos=(0.464, 0.072), resolution=(1334, 750)), vector=[-0.8298, 0.0137], duration=1)
+
+# enter animal collection
+exists(Template(r"tpl1775718110258.png", record_pos=(-0.23, -0.17), resolution=(1334, 750)))
+touch(Template(r"tpl1775718110258.png", record_pos=(-0.23, -0.17), resolution=(1334, 750)))
+swipe(Template(r"tpl1775717971109.png", target_pos=8, record_pos=(0.464, 0.072), resolution=(1334, 750)), vector=[-0.8298, 0.0137], duration=1)
+sleep(1.0)
+
+#
+exists(Template(r"tpl1775718210549.png", record_pos=(-0.147, -0.172), resolution=(1334, 750)))
+touch(Template(r"tpl1775718210549.png", record_pos=(-0.147, -0.172), resolution=(1334, 750)))
+swipe(Template(r"tpl1775717971109.png", target_pos=8, record_pos=(0.464, 0.072), resolution=(1334, 750)), vector=[-0.8298, 0.0137], duration=1)
+sleep(1.0)
+
+
+exists(Template(r"tpl1775718254158.png", record_pos=(-0.064, -0.172), resolution=(1334, 750)))
+touch(Template(r"tpl1775718254158.png", record_pos=(-0.064, -0.172), resolution=(1334, 750)))
+swipe(Template(r"tpl1775717971109.png", target_pos=8, record_pos=(0.464, 0.072), resolution=(1334, 750)), vector=[-0.8298, 0.0137], duration=1)
+sleep(1.0)
+
+#
+exists(Template(r"tpl1775718415466.png", record_pos=(0.017, -0.173), resolution=(1334, 750)))
+touch(Template(r"tpl1775718415466.png", record_pos=(0.017, -0.173), resolution=(1334, 750)))
+swipe(Template(r"tpl1775717971109.png", target_pos=8, record_pos=(0.464, 0.072), resolution=(1334, 750)), vector=[-0.8298, 0.0137], duration=1)
+sleep(1.0)
+
+#
+exists(Template(r"tpl1775718446371.png", record_pos=(0.1, -0.172), resolution=(1334, 750)))
+touch(Template(r"tpl1775718446371.png", record_pos=(0.1, -0.172), resolution=(1334, 750)))
+swipe(Template(r"tpl1775717971109.png", target_pos=8, record_pos=(0.464, 0.072), resolution=(1334, 750)), vector=[-0.8298, 0.0137], duration=1)
+sleep(1.0)
+
+#
+exists(Template(r"tpl1775718472412.png", record_pos=(0.183, -0.172), resolution=(1334, 750)))
+touch(Template(r"tpl1775718472412.png", record_pos=(0.183, -0.172), resolution=(1334, 750)))
+swipe(Template(r"tpl1775717971109.png", target_pos=8, record_pos=(0.464, 0.072), resolution=(1334, 750)), vector=[-0.8298, 0.0137], duration=1)
+sleep(1.0)
+
+#
+exists(Template(r"tpl1775718507027.png", record_pos=(0.265, -0.172), resolution=(1334, 750)))
+touch(Template(r"tpl1775718507027.png", record_pos=(0.265, -0.172), resolution=(1334, 750)))
+swipe(Template(r"tpl1775717971109.png", target_pos=8, record_pos=(0.464, 0.072), resolution=(1334, 750)), vector=[-0.8298, 0.0137], duration=1)
+sleep(1.0)
+
+#
+exists(Template(r"tpl1775718538652.png", record_pos=(0.347, -0.172), resolution=(1334, 750)))
+touch(Template(r"tpl1775718538652.png", record_pos=(0.347, -0.172), resolution=(1334, 750)))
+swipe(Template(r"tpl1775717971109.png", target_pos=8, record_pos=(0.464, 0.072), resolution=(1334, 750)), vector=[-0.8298, 0.0137], duration=1)
+sleep(1.0)
+
+#
+exists(Template(r"tpl1775718567149.png", record_pos=(0.428, -0.173), resolution=(1334, 750)))
+touch(Template(r"tpl1775718567149.png", record_pos=(0.428, -0.173), resolution=(1334, 750)))
+swipe(Template(r"tpl1775717971109.png", target_pos=8, record_pos=(0.464, 0.072), resolution=(1334, 750)), vector=[-0.8298, 0.0137], duration=1)
+
+
+
+
