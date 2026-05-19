9aa9c1384ec3b08d6620cd0ae15e1aae663933d8
gitsupportjog <jog.dmlqa1@gmail.com>
2026-05-19 14:14:21 +0700

    Add files via upload

---
+++ Parental.air/Parental.air/Parental.py
@@ -0,0 +1,34 @@
+# -*- encoding=utf8 -*-
+__author__ = "armadya.hadi"
+
+from airtest.core.api import *
+
+auto_setup(__file__)
+touch(Template(r"tpl1775031036498.png", record_pos=(0.299, -0.179), resolution=(1792, 828)))
+touch(Template(r"tpl1775031085333.png", record_pos=(0.151, -0.048), resolution=(1792, 828)))
+touch(Template(r"tpl1775031094021.png", record_pos=(0.326, 0.102), resolution=(1792, 828)), times=2)
+touch(Template(r"tpl1775031123524.png", record_pos=(0.239, 0.177), resolution=(1792, 828)))
+sleep(3)
+touch(Template(r"tpl1775031213099.png", record_pos=(-0.39, 0.199), resolution=(1792, 828)))
+swipe(Template(r"tpl1775031234832.png", record_pos=(0.057, 0.0), resolution=(1792, 828)), vector=[-0.0025, -0.4094])
+swipe(Template(r"tpl1775031259080.png", record_pos=(0.055, -0.006), resolution=(1792, 828)), vector=[0.0002, -0.4204])
+swipe(Template(r"tpl1775031311265.png", record_pos=(0.035, 0.031), resolution=(1792, 828)), vector=[0.0045, -0.3499])
+sleep(2)
+touch(Template(r"tpl1775031328926.png", record_pos=(-0.173, -0.126), resolution=(1792, 828)))
+sleep(2)
+swipe(Template(r"tpl1775032296220.png", record_pos=(0.377, -0.031), resolution=(1792, 828)), vector=[0.0004, -0.3275])
+swipe(Template(r"tpl1775032296220.png", record_pos=(0.377, -0.031), resolution=(1792, 828)), vector=[0.0004, -0.3275])
+swipe(Template(r"tpl1775032296220.png", record_pos=(0.377, -0.031), resolution=(1792, 828)), vector=[0.0004, -0.3275])
+swipe(Template(r"tpl1775032296220.png", record_pos=(0.377, -0.031), resolution=(1792, 828)), vector=[0.0004, -0.3275])
+touch(Template(r"tpl1775031663543.png", record_pos=(-0.001, 0.186), resolution=(1792, 828)))
+touch(Template(r"tpl1775031684763.png", record_pos=(0.056, -0.127), resolution=(1792, 828)))
+swipe(Template(r"tpl1775031721289.png", record_pos=(-0.021, -0.037), resolution=(1792, 828)), vector=[0.0036, -0.3606])
+swipe(Template(r"tpl1775031771338.png", record_pos=(-0.025, -0.036), resolution=(1792, 828)), vector=[-0.0017, -0.3442])
+touch(Template(r"tpl1775031793610.png", record_pos=(0.001, 0.185), resolution=(1792, 828)))
+
+touch(Template(r"tpl1775031839576.png", record_pos=(0.287, -0.133), resolution=(1792, 828)))
+swipe(Template(r"tpl1775031859669.png", record_pos=(-0.008, -0.04), resolution=(1792, 828)), vector=[0.0067, -0.3413])
+
+swipe(Template(r"tpl1775031909781.png", record_pos=(-0.031, -0.034), resolution=(1792, 828)), vector=[0.0014, -0.3645])
+touch(Template(r"tpl1775031965509.png", record_pos=(-0.002, 0.183), resolution=(1792, 828)))
+touch(Template(r"tpl1775032041951.png", record_pos=(-0.392, -0.19), resolution=(1792, 828)))
