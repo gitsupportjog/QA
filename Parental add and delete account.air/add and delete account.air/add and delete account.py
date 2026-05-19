ee0c24708567223620b90e1c97879b82d399e85c
gitsupportjog <jog.dmlqa1@gmail.com>
2026-05-19 08:43:03 +0700

    Add files via upload

---
+++ Parental add and delete account.air/add and delete account.air/add and delete account.py
@@ -0,0 +1,32 @@
+# -*- encoding=utf8 -*-
+__author__ = "USER"
+
+from airtest.core.api import *
+
+auto_setup(__file__)
+touch(Template(r"tpl1773643854162.png", record_pos=(0.298, -0.18), resolution=(1792, 828)))
+touch(Template(r"tpl1773643873174.png", record_pos=(0.15, -0.047), resolution=(1792, 828)))
+touch(Template(r"tpl1773643880262.png", record_pos=(0.324, 0.103), resolution=(1792, 828)), times=2)
+touch(Template(r"tpl1773643894182.png", record_pos=(0.239, 0.176), resolution=(1792, 828)))
+touch(Template(r"tpl1773643916111.png", record_pos=(-0.367, -0.09), resolution=(1792, 828)))
+touch(Template(r"tpl1773643935696.png", record_pos=(0.055, -0.008), resolution=(1792, 828)))
+touch(Template(r"tpl1773643965137.png", record_pos=(-0.007, -0.002), resolution=(1792, 828)))
+touch(Template(r"tpl1773643980153.png", record_pos=(-0.026, -0.094), resolution=(1792, 828)))
+text("c")
+touch(Template(r"tpl1773644004865.png", record_pos=(0.081, 0.175), resolution=(1792, 828)))
+touch(Template(r"tpl1773647339526.png", record_pos=(0.0, -0.008), resolution=(1792, 828)))
+touch(Template(r"tpl1773647353990.png", record_pos=(0.256, -0.162), resolution=(1792, 828)))
+touch(Template(r"tpl1773647417519.png", record_pos=(-0.391, -0.19), resolution=(1792, 828)))
+sleep(3)
+touch(Template(r"tpl1773643854162.png", record_pos=(0.298, -0.18), resolution=(1792, 828)))
+touch(Template(r"tpl1773643873174.png", record_pos=(0.15, -0.047), resolution=(1792, 828)))
+touch(Template(r"tpl1773643880262.png", record_pos=(0.324, 0.103), resolution=(1792, 828)), times=2)
+touch(Template(r"tpl1773643894182.png", record_pos=(0.239, 0.176), resolution=(1792, 828)))
+sleep(2)
+touch(Template(r"tpl1773643916111.png", record_pos=(-0.367, -0.09), resolution=(1792, 828)))
+touch(Template(r"tpl1773647493114.png", target_pos=7, record_pos=(-0.004, -0.002), resolution=(1792, 828)))
+
+touch(Template(r"tpl1773647532323.png", record_pos=(-0.05, 0.176), resolution=(1792, 828)))
+touch(Template(r"tpl1773647545292.png", record_pos=(-0.071, 0.035), resolution=(1792, 828)))
+touch(Template(r"tpl1773647353990.png", record_pos=(0.256, -0.162), resolution=(1792, 828)))
+touch(Template(r"tpl1773647417519.png", record_pos=(-0.391, -0.19), resolution=(1792, 828)))
