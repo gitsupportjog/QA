10e51df7f8c8db0cdf821b0c441a2bcf62d50c23
jindaltenan <urusanperjindalan@gmail.com>
2026-05-22 16:30:26 +0700

    Add files via upload

---
+++ FTUE and Scan.air/FTUE and Scan.py
@@ -0,0 +1,78 @@
+# -*- encoding=utf8 -*-
+__author__ = "yordan.ridhanto"
+
+from airtest.core.api import *
+
+auto_setup(__file__)
+stop_app("com.gamma.scan")
+stop_app("com.ferrero.applayduGP")
+clear_app("com.ferrero.applayduGP")
+start_app("com.ferrero.applayduGP")
+wait(Template(r"tpl1768375365332.png", record_pos=(-0.006, -0.12), resolution=(2340, 1080)))
+touch(Template(r"tpl1768377627025.png", record_pos=(-0.351, 0.177), resolution=(2340, 1080)))
+touch(Template(r"tpl1768377636730.png", record_pos=(0.188, -0.05), resolution=(2340, 1080)))
+touch(Template(r"tpl1768377643474.png", record_pos=(0.368, 0.098), resolution=(2340, 1080)))
+touch(Template(r"tpl1768377651208.png", record_pos=(0.366, 0.099), resolution=(2340, 1080)))
+touch(Template(r"tpl1768377657332.png", record_pos=(0.367, -0.047), resolution=(2340, 1080)))
+wait(Template(r"tpl1768377677614.png", record_pos=(-0.273, -0.048), resolution=(2340, 1080)))
+touch(Template(r"tpl1768377688127.png", record_pos=(-0.236, 0.177), resolution=(2340, 1080)))
+wait(Template(r"tpl1768377691903.png", record_pos=(-0.241, -0.007), resolution=(2340, 1080)))
+touch(Template(r"tpl1768377697508.png", record_pos=(-0.082, 0.179), resolution=(2340, 1080)))
+wait(Template(r"tpl1768377703662.png", record_pos=(-0.311, -0.043), resolution=(2340, 1080)))
+touch(Template(r"tpl1768377709306.png", record_pos=(0.416, 0.173), resolution=(2340, 1080)))
+touch(Template(r"tpl1768377718758.png", record_pos=(0.404, 0.177), resolution=(2340, 1080)))
+touch(Template(r"tpl1768377727230.png", record_pos=(-0.215, -0.049), resolution=(2340, 1080)))
+touch(Template(r"tpl1768377730959.png", record_pos=(-0.213, -0.01), resolution=(2340, 1080)))
+touch(Template(r"tpl1768377733746.png", record_pos=(-0.213, 0.029), resolution=(2340, 1080)))
+touch(Template(r"tpl1768377737176.png", record_pos=(-0.216, 0.066), resolution=(2340, 1080)))
+touch(Template(r"tpl1768377741824.png", record_pos=(0.056, -0.037), resolution=(2340, 1080)))
+touch(Template(r"tpl1768377748888.png", record_pos=(0.4, 0.179), resolution=(2340, 1080)))
+touch(Template(r"tpl1768377762320.png", record_pos=(-0.072, 0.185), resolution=(2340, 1080)))
+touch(Template(r"tpl1768377770199.png", record_pos=(-0.408, -0.185), resolution=(2340, 1080)))
+touch(Template(r"tpl1768377779626.png", record_pos=(0.068, 0.187), resolution=(2340, 1080)))
+touch(Template(r"tpl1768377786076.png", record_pos=(-0.409, -0.185), resolution=(2340, 1080)))
+touch(Template(r"tpl1768377793490.png", record_pos=(-0.391, 0.187), resolution=(2340, 1080)))
+sleep(4.0)
+touch(Template(r"tpl1768377891481.png", record_pos=(-0.047, 0.076), resolution=(2340, 1080)))
+touch(Template(r"tpl1768378031044.png", record_pos=(-0.041, 0.147), resolution=(2340, 1080)), duration=2)
+touch(Template(r"tpl1768378067525.png", record_pos=(-0.233, 0.144), resolution=(2340, 1080)), duration=1)
+touch(Template(r"tpl1768378084186.png", record_pos=(0.46, 0.19), resolution=(2340, 1080)))
+touch((0.477, 0.875))
+text("Anjay")
+touch(Template(r"tpl1768378330015.png", record_pos=(0.413, 0.174), resolution=(2340, 1080)))
+wait(Template(r"tpl1768378343228.png", record_pos=(0.022, -0.159), resolution=(2340, 1080)))
+touch(Template(r"tpl1768378347860.png", record_pos=(-0.097, 0.119), resolution=(2340, 1080)))
+sleep(1.0)
+touch(Template(r"tpl1768378364806.png", record_pos=(0.15, 0.037), resolution=(2340, 1080)))
+sleep(2.0)
+swipe(Template(r"tpl1768378630068.png", record_pos=(0.138, 0.023), resolution=(2340, 1080)), vector=[-0.0043, -0.352])
+swipe(Template(r"tpl1768378767538.png", record_pos=(0.164, -0.003), resolution=(2340, 1080)), vector=[0.0042, -0.4135])
+sleep(8.0)
+keyevent("HOME")
+sleep(1.0)
+start_app("com.gamma.scan")
+wait(Template(r"tpl1768381059307.png", record_pos=(-0.169, -0.908), resolution=(1080, 2340)))
+touch(Template(r"tpl1768381064935.png", record_pos=(-0.162, -0.903), resolution=(1080, 2340)))
+wait(Template(r"tpl1768381138363.png", record_pos=(-0.3, -0.881), resolution=(1080, 2340)))
+touch(Template(r"tpl1768381144998.png", record_pos=(-0.33, -0.456), resolution=(1080, 2340)))
+wait(Template(r"tpl1768381153744.png", record_pos=(-0.255, -0.883), resolution=(1080, 2340)))
+touch(Template(r"tpl1768381159358.png", record_pos=(-0.369, -0.666), resolution=(1080, 2340)))
+wait(Template(r"tpl1768381168409.png", record_pos=(0.007, -0.912), resolution=(1080, 2340)))
+touch((0.33, 0.201))
+sleep(3.0)
+touch(Template(r"tpl1768381412263.png", record_pos=(0.199, 0.029), resolution=(2340, 1080)))
+wait(Template(r"tpl1768381438686.png", record_pos=(0.022, 0.179), resolution=(2340, 1080)))
+touch(Template(r"tpl1768381444725.png", record_pos=(0.024, 0.18), resolution=(2340, 1080)))
+
+
+
+
+
+
+
+
+
+
+
+
+
